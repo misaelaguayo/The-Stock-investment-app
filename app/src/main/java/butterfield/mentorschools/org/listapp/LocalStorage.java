6266e1cd2ee0cd72ac6e2ba445eebef03cde9474
Misael <aguayo.misael@gmail.com>
2018-11-18 21:20:51 -0600

    Followed companies being populated on main screen. Does not yet refresh.

--- app/src/main/java/butterfield/mentorschools/org/listapp/LocalStorage.java
+++ app/src/main/java/butterfield/mentorschools/org/listapp/LocalStorage.java
@@ -3,9 +3,17 @@ package butterfield.mentorschools.org.listapp;
 import android.app.NotificationChannel;
 import android.content.Context;
 import android.util.Log;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class LocalStorage {
 
@@ -16,11 +24,49 @@ public class LocalStorage {
             OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("LocalStorage.txt",context.MODE_APPEND));
             outputStreamWriter.write(data);
             outputStreamWriter.close();
+            Toast.makeText(context,"Saved to " + context.getFilesDir() + "/" + "LocalStorage.txt", Toast.LENGTH_LONG).show();
         }
         catch(IOException e)
         {
             Log.e("Writing exception","File write failed: " + e.toString());
         }
+    }
+
+    public String[] load(Context context)
+    {
+        String ret = "";
+        List<String> companiesList = new ArrayList<String>();
+
+        try {
+            InputStream inputStream = context.openFileInput("LocalStorage.txt");
+
+            if(inputStream!= null){
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ((receiveString = bufferedReader.readLine()) != null){
+                    companiesList.add(receiveString);
+                }
+
+                inputStream.close();
+            }
+            String[] companies = new String[companiesList.size()];
+            companies = companiesList.toArray(companies);
+            return companies;
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+
+        return null;
     }
 
 }
--- app/src/main/java/butterfield/mentorschools/org/listapp/MainActivity.java
+++ app/src/main/java/butterfield/mentorschools/org/listapp/MainActivity.java
@@ -6,9 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     //Testing pull request
@@ -16,17 +19,34 @@ public class MainActivity extends AppCompatActivity {
     Button go;
     EditText searchBar;
     String searchText;
+    String[] loadCompanies;
+    ArrayAdapter<String> adapter;
+    ArrayList<String> arrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Resources res = getResources();
-        myListView = (ListView)findViewById(R.id.myListView);
 
+        myListView = (ListView)findViewById(R.id.myListView);
         searchBar = (EditText) findViewById(R.id.searchBar);
         go = (Button) findViewById(R.id.button);
+
+        try {
+            loadCompanies = new LocalStorage().load(getApplicationContext());
+            arrayList = new ArrayList<String>();
+            adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item, arrayList);
+            myListView.setAdapter(adapter);
+
+            for (int i = 0; i < loadCompanies.length; i++) {
+                arrayList.add(loadCompanies[i]);
+            }
+            adapter.notifyDataSetChanged();
+        }
+        catch(NullPointerException e){
+
+        }
 
         go.setOnClickListener(new View.OnClickListener() {
             @Override
